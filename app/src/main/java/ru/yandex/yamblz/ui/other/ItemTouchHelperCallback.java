5c08fb1604e3efc97ae9a2c668f8cfb883b6bee3
Kostya Charkin <93kostya@gmail.com>
2016-07-30 23:31:30 +0300

    add red background when swipe item

--- app/src/main/java/ru/yandex/yamblz/ui/other/ItemTouchHelperCallback.java
+++ app/src/main/java/ru/yandex/yamblz/ui/other/ItemTouchHelperCallback.java
@@ -1,8 +1,12 @@
 package ru.yandex.yamblz.ui.other;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
+import android.view.View;
 
 /**
  * Created by kostya on 30.07.16.
@@ -12,10 +16,13 @@ public class ItemTouchHelperCallback extends ItemTouchHelper.Callback {
     private final ItemTouchHelperAdapter mAdapter;
     private LastMovedDecoration lastMovedDecoration;
     private boolean isMoving = false;
+    Paint paintForSwipe;
 
     public ItemTouchHelperCallback(ItemTouchHelperAdapter mAdapter, LastMovedDecoration lastMovedDecoration) {
         this.mAdapter = mAdapter;
         this.lastMovedDecoration = lastMovedDecoration;
+        paintForSwipe = new Paint();
+        paintForSwipe.setColor(Color.RED);
     }
 
     @Override
@@ -56,4 +63,18 @@ public class ItemTouchHelperCallback extends ItemTouchHelper.Callback {
         return true;
     }
 
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+            View child = viewHolder.itemView;
+            int width = child.getWidth();
+            paintForSwipe.setAlpha((int) (255 * Math.abs(dX) / width));
+            if (dX > 0) {
+                c.drawRect(child.getLeft(), child.getTop(), dX, child.getBottom(), paintForSwipe);
+            } else {
+                c.drawRect(width + dX, child.getTop(), child.getRight(), child.getBottom(), paintForSwipe);
+            }
+        }
+    }
 }
